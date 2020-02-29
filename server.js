9406b42266f3600f5bc19a1a4d992c64af15d497
Rene Goldschmid <renegoldschmid@gmail.com>
2020-02-29 12:14:50 +0100

    Data cleanup and converted to json

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express');
+const data = require('./service/data');
 const app = express();
 
 app.use(express.json());
 
+// FOR TESTING
+const testData = [1,2,3,4,5,6,7];
+
 // Root-Route
 app.get('/', (req, res) => {
+  //const { numbers } = req.body;
+
   res.json('Hello World!');
+
 });
 
 
---
+++ service/data.js
@@ -0,0 +1,4 @@
+const path = require('path');
+
+const csvFilePath = path.resolve('./data/2017.json');
+
