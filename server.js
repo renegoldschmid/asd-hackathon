b9dc276e3e1218248332dbc460229d544e300a7b
Rene Goldschmid <renegoldschmid@gmail.com>
2020-02-29 14:23:24 +0100

    Implementing highest match

--- server.js
+++ server.js
@@ -1,18 +1,75 @@
 const express = require('express');
-const data = require('./service/data');
+const dataManager = require('./service/data');
 const app = express();
 
 app.use(express.json());
 
+let responseObject = {
+  highestMatch: 0,
+  amountWon: 0,
+  bestFittingNumbers: [],
+  date: ''
+}
+
 // FOR TESTING
-const testData = [1,2,3,4,5,6,7];
+const testData = [1,2,3,13,26,28,35];
+
+// Extract Numbers
+const checkMatchingNumbers = (userNumbers, dataset) => {
+  let matchedNumbers, highestMatch = 0;
+  let matchingIndex = -1;
+  let zusatzzahl = false;
+
+  // Dataset loop
+  for (var i = 0; i < dataset.length; i++) {
+    if (matchedNumbers !== 0 && highestMatch < matchedNumbers) {
+      highestMatch = matchedNumbers;
+      matchingIndex = i - 1; // Previous index needed
+    }
+    matchedNumbers = 0; // Reset counter for matching numbers
+
+    // Row Loop
+    for (const [key, value] of Object.entries(dataset[i])) {
+
+      if (i % 2 === 0) {
+        // Lottozahlen stehen immer in einer geraden Zeile
+        // console.log(i, key);
+        if (key.indexOf('Zahl') === 0) {
+          if (userNumbers.includes(value)) {
+            console.log('test');
+            matchedNumbers++;
+          }
+        }
+
+        if (key === "Zusatzzahl" && value === userNumbers[6]) {
+          zusatzzahl = true;
+        } 
+      }
+    }
+  }
+
+  return ({
+    highestMatch: highestMatch,
+    matchingIndex: matchingIndex,
+    zusatzzahl: zusatzzahl
+  });
+}
 
 // Root-Route
 app.get('/', (req, res) => {
-  //const { numbers } = req.body;
+  //const { userNumbers } = req.body;
+  const userNumbers = testData;
 
-  res.json('Hello World!');
+  let calculatedResponse = checkMatchingNumbers(userNumbers, dataManager.dataOfTest);
 
+  if (calculatedResponse.highestMatch >= 3) {
+
+  }
+  // responseObject = {...responseObject, 
+  //   highestMatch: highestMatch
+  // };
+
+  res.json(calculatedResponse);
 });
 
 
--- service/data.js
+++ service/data.js
@@ -1,4 +1,13 @@
 const path = require('path');
 
-const csvFilePath = path.resolve('./data/2017.json');
+const data2017 = require(path.resolve('./data/2017.json'));
+const data2018 = require(path.resolve('./data/2017.json'));
+const data2019 = require(path.resolve('./data/2017.json'));
 
+const dataTest = require(path.resolve('./data/test.json'));
+
+module.exports.dataOf2017 = data2017;
+module.exports.dataOf2018 = data2018;
+module.exports.dataOf2019 = data2019;
+
+module.exports.dataOfTest = dataTest;
