5301e05465b1a3474be84dcc7e681b4c8f57f637
Rene Goldschmid <renegoldschmid@gmail.com>
2020-02-29 15:52:02 +0100

    Finished server functionalities

--- server.js
+++ server.js
@@ -1,44 +1,37 @@
 
 
 const express = require('express');
+const cors = require('cors');
 const dataManager = require('./service/data');
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
-let responseObject = {
-  highestMatch: 0,
-  amountWon: 0,
-  bestFittingNumbers: [],
-  date: ''
-}
-
-// FOR TESTING
-const testData = [1,2,3,13,26,28,35];
-
-// Extract Numbers
+// Check for the best matching numbers
 const checkMatchingNumbers = (userNumbers, dataset) => {
   let matchedNumbers, highestMatch = 0;
   let matchingIndex = -1;
-  let zusatzzahl = false;
+  let zusatzzahl, zusatzzahlWithMatch = false;
 
   // Dataset loop
   for (var i = 0; i < dataset.length; i++) {
     if (matchedNumbers !== 0 && highestMatch < matchedNumbers) {
       highestMatch = matchedNumbers;
+      zusatzzahlWithMatch = zusatzzahl; 
       matchingIndex = i - 1; // Previous index needed
     }
     matchedNumbers = 0; // Reset counter for matching numbers
+    zusatzzahl = false; // reset zusatzzahl
 
     // Row Loop
     for (const [key, value] of Object.entries(dataset[i])) {
 
       if (i % 2 === 0) {
         // Lottozahlen stehen immer in einer geraden Zeile
-        // console.log(i, key);
+
         if (key.indexOf('Zahl') === 0) {
           if (userNumbers.includes(value)) {
-            console.log('test');
             matchedNumbers++;
           }
         }
@@ -53,25 +46,110 @@ const checkMatchingNumbers = (userNumbers, dataset) => {
   return ({
     highestMatch: highestMatch,
     matchingIndex: matchingIndex,
-    zusatzzahl: zusatzzahl
+    zusatzzahl: zusatzzahlWithMatch
   });
+}
+
+// Check the prize according to the best matching numbers
+const getPrize = (calculatedResponse, dataset) => {
+  let calculatedPrize = {};
+  let data;
+  
+  if (calculatedResponse.highestMatch > 4 || (calculatedResponse.highestMatch === 4 && calculatedResponse.zusatzzahl)) {
+    console.log('erster index', calculatedResponse.matchingIndex);
+    data = dataset[calculatedResponse.matchingIndex];
+  } else if ((calculatedResponse.highestMatch < 4 && calculatedResponse.highestMatch > 2) 
+    || (calculatedResponse.highestMatch === 4 && !calculatedResponse.zusatzzahl)) {
+    console.log('zweiter index', calculatedResponse.matchingIndex);
+    data = dataset[calculatedResponse.matchingIndex];
+    dataPrize = dataset[calculatedResponse.matchingIndex + 1];
+  } else {
+    return {};
+  }
+
+  console.log(data);
+
+  switch(calculatedResponse.highestMatch) {
+    case 3:
+      if (calculatedResponse.zusatzzahl) {
+        calculatedPrize = {
+          bestFittingNumbers: [data.Zahl1, data.Zahl2, data.Zahl3, data.Zahl4, data.Zahl5, data.Zahl6, data.Zusatzzahl],
+          amountWon: dataPrize.Quote_2_6,
+          date: data.date
+        }
+      } else {
+        calculatedPrize = {
+          bestFittingNumbers: [data.Zahl1, data.Zahl2, data.Zahl3, data.Zahl4, data.Zahl5, data.Zahl6, data.Zusatzzahl],
+          amountWon: dataPrize.Quote_3_7,
+          date: data.date
+        }
+      }
+      break;
+    case 4:
+      if (calculatedResponse.zusatzzahl) {
+        calculatedPrize = {
+          bestFittingNumbers: [data.Zahl1, data.Zahl2, data.Zahl3, data.Zahl4, data.Zahl5, data.Zahl6, data.Zusatzzahl],
+          amountWon: dataPrize.Quote_4_8,
+          date: data.date
+        }
+      } else {
+        calculatedPrize = {
+          bestFittingNumbers: [data.Zahl1, data.Zahl2, data.Zahl3, data.Zahl4, data.Zahl5, data.Zahl6, data.Zusatzzahl],
+          amountWon: data.Quote_1_5,
+          date: data.date
+        }
+      }
+      break;
+    case 5:
+      if (calculatedResponse.zusatzzahl) {
+        calculatedPrize = {
+          bestFittingNumbers: [data.Zahl1, data.Zahl2, data.Zahl3, data.Zahl4, data.Zahl5, data.Zahl6, data.Zusatzzahl],
+          amountWon: data.Quote_2_6,
+          date: data.date
+        }
+      } else {
+        calculatedPrize = {
+          bestFittingNumbers: [data.Zahl1, data.Zahl2, data.Zahl3, data.Zahl4, data.Zahl5, data.Zahl6, data.Zusatzzahl],
+          amountWon: data.Quote_3_7,
+          date: data.date
+        }
+      }
+      break;
+    case 6:
+      calculatedPrize = {
+        bestFittingNumbers: [data.Zahl1, data.Zahl2, data.Zahl3, data.Zahl4, data.Zahl5, data.Zahl6, data.Zusatzzahl],
+        amountWon: data.Quote_1_5,
+        date: data.date
+      }
+      break;
+    default:
+      // todo
+      console.log('Nix');
+  }
+
+  return calculatedPrize;
 }
 
 // Root-Route
 app.get('/', (req, res) => {
-  //const { userNumbers } = req.body;
-  const userNumbers = testData;
+  const numbers = req.query.numbers;
+  console.log(req.query.numbers);
 
-  let calculatedResponse = checkMatchingNumbers(userNumbers, dataManager.dataOfTest);
+  let calculatedMatchingNumbers = checkMatchingNumbers(numbers, dataManager.dataOf2019);
+  let calculatedPrize = getPrize(calculatedMatchingNumbers, dataManager.dataOf2019);
+  
+  let date = new Date();
+  let serverResponse = {
+    data: {
+      lastAppearence: '',
+      bestFittingNumbers: calculatedPrize.bestFittingNumbers,
+      propability: 1,
+      winAmount: calculatedPrize.amountWon,
+    },
+    success: true
+  };
 
-  if (calculatedResponse.highestMatch >= 3) {
-
-  }
-  // responseObject = {...responseObject, 
-  //   highestMatch: highestMatch
-  // };
-
-  res.json(calculatedResponse);
+  res.json(serverResponse);
 });
 
 
